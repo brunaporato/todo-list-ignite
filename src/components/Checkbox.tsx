c97f53b2c8137c0a6964ae3ef5bbf83d8c7ba54f
Bruna (Depto. Ed.) <brunaporato@gmail.com>
2023-10-18 16:43:23 -0300

    fix checkbox comp

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,9 +1,13 @@
 import styles from "./Checkbox.module.css";
 
-export function Checkbox() {
+interface Props {
+  checked?: boolean;
+}
+
+export function Checkbox({checked = false}: Props) {
     return (
       <label className={styles.container}>
-        <input type="checkbox" className={styles.checkbox}/>
+        <input type="checkbox" className={styles.checkbox} checked={checked}/>
         <span className={styles.checkmark} />
       </label>
     )
