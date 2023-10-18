4e0657cc57262b54cb7ac61e455f413e117194d3
Bruna <brunaporato@gmail.com>
2023-10-18 09:33:41 -0300

    add component input

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Header } from "./components/Header";
+import { Input } from "./components/Input";
 import "./global.css";
 
 export function App() {
   return (
     <div>
       <Header />
+      <Input />
     </div>
   )
 }
