b31af919f74414ba66a05ad75d4bb302e066ea59
Bruna (Depto. Ed.) <brunaporato@gmail.com>
2023-10-19 12:35:30 -0300

    layout adjustments at app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,14 @@ import { Input } from "./components/Input";
 import { BttnCreate } from "./components/BttnCreate";
 import { Task } from "./components/Task";
 
+import clipboard from "./assets/clipboard.svg";
+
 import styles from "./App.module.css";
 import "./global.css";
 
 export function App() {
+  let tasks: Object[] = [];
+
   return (
     <div className={styles.page}>
       <Header />
@@ -15,7 +19,29 @@ export function App() {
           <Input />
           <BttnCreate />
         </div>
-        <Task text="Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer." />
+        <main className={styles.main}>
+          <div className={styles.mainTop}>
+            <div className={styles.created}>
+              <p>Tarefas criadas</p>
+              <span>0</span>
+            </div>
+            <div className={styles.done}>
+              <p>Concluídas</p>
+              <span>0</span>
+            </div>
+          </div>
+          <div className={tasks.length == 0 ? styles.hide : styles.tasks}>
+          </div>
+          <div className={tasks.length == 0 ? styles.tasksNull : styles.hide}>
+            <img src={clipboard} alt="clipboard icon" />
+            <p>
+              Você ainda não tem tarefas cadastradas <br />
+              <span>
+                Crie tarefas e organize seus itens a fazer
+              </span>
+            </p>
+          </div>
+        </main>
       </div>
     </div>
   )
